334691b070ebcf440c453d7d81a94f148c8e03c0
equilibrium <jorgemdcosta@gmail.com>
2025-10-21 00:33:32 +0100

    Add basic statistics module with mean, median and mode functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use rust_data_utils::statistics::*;
+
 fn main() {
-    println!("Hello, world!");
+    let data = vec![3, 2, 5, 1, 4, 2, 2, 2, 5];
+
+    let mean = mean(&data);
+
+    println!("mean: {}", mean);
+
+    let median = median(&data);
+
+    println!("median: {}", median);
+
+    let median = moda(&data);
+
+    println!("median: {}", median);
 }
---
+++ src/statistics/basic.rs
@@ -0,0 +1,44 @@
+use std::collections::HashMap;
+
+pub fn mean(data: &[i32]) -> f64 {
+    if data.is_empty() {
+        return 0.0;
+    }
+
+    let sum: i32 = data.iter().sum();
+
+    sum as f64 / data.len() as f64
+}
+
+pub fn median(data: &[i32]) -> i32 {
+    if data.is_empty() {
+        return 0;
+    }
+
+    let mut data_copy = data.to_vec();
+    data_copy.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+    let len = data.len();
+    if len % 2 == 0 {
+        let m1 = data_copy[len / 2];
+        let m2 = data_copy[len / 2 - 1];
+
+        (m1 + m2) / 2
+    } else {
+        data_copy[len / 2]
+    }
+}
+
+pub fn moda(data: &[i32]) -> i32 {
+    let mut map: HashMap<i32, i32> = HashMap::new();
+
+    for v in data.iter() {
+        let count = map.entry(*v).or_insert(0);
+        *count += 1;
+    }
+
+    map.into_iter()
+        .max_by_key(|(_, count)| *count)
+        .map(|(v, _)| v)
+        .unwrap()
+}
